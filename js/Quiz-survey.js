7f9fe90a8c20691ae7778d603a86fd4f9ec15469
Brandon Nolan <brndalx@gmail.com>
2022-12-24 23:02:22 +0200

    fix: logic of quiz improved

--- js/Quiz-survey.js
+++ js/Quiz-survey.js
@@ -1,7 +1,3 @@
-window.onload = function () {
-  // Your code here
-};
-
 document.querySelectorAll(".form-quiz-option").forEach(function (element) {
   element.addEventListener("click", function (event) {
     if (event.target.classList.contains("form-check-input")) {
@@ -40,6 +36,7 @@ function quiz() {
   //text question
   let question = document.querySelector("#id_question");
   let modalhead = document.getElementById("exampleModalLabel");
+  let modalparagraph = document.getElementById("modal-paragraph");
   //text answer
   let answer1 = document.querySelector("#text-option-1");
   let answer2 = document.querySelector("#text-option-2");
@@ -104,6 +101,7 @@ function quiz() {
       answer2.innerHTML = "Satisfied";
       answer3.innerHTML = "Neutral";
       answer4.innerHTML = "Unsatisfied";
+
       break;
 
     case 7:
@@ -124,31 +122,36 @@ function quiz() {
         counterOpt1 > counterOpt4
       ) {
         modalhead.innerHTML =
-          "We are thrilled to hear that you had a fantastic experience with our online banking services. We are committed to providing top-quality service and will continue to work on continuous improvement to ensure that our customers have the best possible experience with us.";
-        modalhead.style.color = "green";
+          "We are thrilled to hear that you had a fantastic experience with our online banking services.";
+
+        modalparagraph.innerHTML =
+          "We are committed to providing top-quality service and will continue to work on continuous improvement to ensure that our customers have the best possible experience with us.";
       } else if (
         counterOpt2 > counterOpt1 &&
         counterOpt2 > counterOpt3 &&
         counterOpt2 > counterOpt4
       ) {
         modalhead.innerHTML =
-          "Thank you for taking the time to share your thoughts about our online banking services. We value all feedback and will use it to continue improving and updating our offerings to better serve our customers.";
-        modalhead.style.color = "green";
+          "Thank you for taking the time to share your thoughts about our online banking services.";
+        modalparagraph.innerHTML =
+          "We value all feedback and will use it to continue improving and updating our offerings to better serve our customers.";
       } else if (
         counterOpt3 > counterOpt1 &&
         counterOpt3 > counterOpt2 &&
         counterOpt3 > counterOpt4
       ) {
         modalhead.innerHTML =
-          "hank you for your feedback on our online banking services. We are glad to hear that you were satisfied with your experience. We strive to provide reliable and efficient service to all of our customers, and we appreciate your feedback as we continue to work on improving and updating our offerings.";
-        modalhead.style.color = "green";
+          "Thank you for your feedback on our online banking services. We are glad to hear that you were satisfied with your experience.";
+        modalparagraph.innerHTML =
+          "We strive to provide reliable and efficient service to all of our customers, and we appreciate your feedback as we continue to work on improving and updating our offerings.";
       } else {
         modalhead.innerHTML =
-          "We apologize for the subpar experience you had with our online banking services. We understand how important it is to have reliable and efficient service, and we will work diligently on continuous improvement to ensure that our customers receive the best possible service in the future.";
-        question.style.color = "green";
+          "We apologize for the subpar experience you had with our online banking services.";
+        modalparagraph.innerHTML =
+          "We understand how important it is to have reliable and efficient service, and we will work diligently on continuous improvement to ensure that our customers receive the best possible service in the future.";
       }
 
-      button.innerHTML = "Start Again?";
+      button.innerHTML = "Finish?";
 
       break;
 
