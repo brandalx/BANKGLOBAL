8004f65474d02c5a64ba7f8c0ee0d62e43d88e4e
Brandon Nolan <brndalx@gmail.com>
2023-01-07 17:15:00 +0200

    last commit: AOS initialization fixed

--- js/preloaderwb.js
+++ js/preloaderwb.js
@@ -1,12 +1,13 @@
 document.body.style.overflow = "hidden";
-const pMain = document.getElementById("page-main")
-pMain.style.display = "none"
- function newinit () {
-    setTimeout(() => {
-        document.querySelector('.preloaderwb').style.display = 'none';
-        pMain.style.display = "block"
-        document.body.style.overflow = "auto";
-    }, 2000)
+const pMain = document.getElementById("page-main");
+pMain.style.display = "none";
+function newinit() {
+  setTimeout(() => {
+    document.querySelector(".preloaderwb").style.display = "none";
+    pMain.style.display = "block";
+    document.body.style.overflow = "auto";
+    AOS.init();
+  }, 2000);
 }
 
 newinit();
